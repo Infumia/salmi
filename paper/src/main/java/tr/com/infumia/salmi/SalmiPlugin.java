2db03955de7dc58e1df96eab0050f7c08493bec6
Hasan Demirtaş <utsukushihito@outlook.com>
2022-06-18 11:55:16 +0300

    Update SalmiPlugin.java

--- paper/src/main/java/tr/com/infumia/salmi/SalmiPlugin.java
+++ paper/src/main/java/tr/com/infumia/salmi/SalmiPlugin.java
@@ -1,10 +1,14 @@
 package tr.com.infumia.salmi;
 
+import java.util.List;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 import tr.com.infumia.salmi.api.Redis;
 import tr.com.infumia.salmi.api.SalmiApi;
 import tr.com.infumia.salmi.api.SalmiConfig;
+import tr.com.infumia.salmi.api.User;
 
 public final class SalmiPlugin extends JavaPlugin {
 
@@ -15,9 +19,14 @@ public final class SalmiPlugin extends JavaPlugin {
     Bukkit.getScheduler().runTaskTimerAsynchronously(this, this::updateTabList, 20L, 20L * 3L);
   }
 
+  private void sendPacket(
+    @NotNull final Player player,
+    @NotNull final List<User> users
+  ) {
+  }
+
   private void updateTabList() {
     final var players = Bukkit.getOnlinePlayers();
-    SalmiApi.onlineUsers().thenAccept(users -> {
-    });
+    SalmiApi.onlineUsers().thenAccept(users -> players.forEach(player -> this.sendPacket(player, users)));
   }
 }
