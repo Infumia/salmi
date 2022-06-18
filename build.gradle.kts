f10551489319f61386b75872b6ba8db60cb9f15d
Hasan Demirtaş <utsukushihito@outlook.com>
2022-06-18 11:23:27 +0300

    development. (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,6 @@ plugins {
   id("com.github.johnrengelman.shadow") version "7.1.2" apply false
 }
 
-val signRequired = !rootProject.property("dev").toString().toBoolean()
-
 allprojects {
   group = "tr.com.infumia"
 }
@@ -16,139 +14,37 @@ allprojects {
 subprojects {
   apply<JavaPlugin>()
   apply<JavaLibraryPlugin>()
-
-  if (isJar()) {
-    java {
-      toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-      }
-    }
-
-    tasks {
-      compileJava {
-        options.encoding = Charsets.UTF_8.name()
-      }
-
-      jar {
-        define()
-      }
-
-      build {
-        dependsOn(jar)
-      }
-    }
-
-    repositories {
-      mavenCentral()
-      maven(SNAPSHOTS)
-      maven(PAPERMC)
-      maven(SPONGEPOWERED)
-      mavenLocal()
-    }
-
-    dependencies {
-      compileOnly(lombokLibrary)
-      compileOnly(annotationsLibrary)
-      compileOnly(velocityLibrary)
-
-      annotationProcessor(lombokLibrary)
-      annotationProcessor(annotationsLibrary)
-
-      testImplementation(lombokLibrary)
-      testImplementation(annotationsLibrary)
-
-      testAnnotationProcessor(lombokLibrary)
-      testAnnotationProcessor(annotationsLibrary)
-    }
-  }
-
-  if (isPlugin()) {
-    tasks {
-      processResources {
-        duplicatesStrategy = DuplicatesStrategy.INCLUDE
-        from(project.the<SourceSetContainer>()["main"].resources.srcDirs) {
-          expand("pluginVersion" to project.version)
-          include("velocity-plugin.json")
-          include("plugin.yml")
-        }
-      }
-    }
-  }
-
   if (isPublishing()) {
-    apply {
-      plugin("maven-publish")
-      plugin("signing")
+    apply<MavenPublishPlugin>()
+    apply<SigningPlugin>()
+  }
+
+  java {
+    toolchain {
+      languageVersion.set(JavaLanguageVersion.of(17))
+    }
+  }
+
+  tasks {
+    compileJava {
+      options.encoding = Charsets.UTF_8.name()
     }
 
-    tasks {
-      javadoc {
-        options.encoding = Charsets.UTF_8.name()
-        (options as StandardJavadocDocletOptions).tags("todo")
-      }
-
-      val javadocJar by creating(Jar::class) {
-        dependsOn("javadoc")
-        define(classifier = "javadoc")
-        from(javadoc)
-      }
-
-      val sourcesJar by creating(Jar::class) {
-        dependsOn("classes")
-        define(classifier = "sources")
-        from(sourceSets["main"].allSource)
-      }
-
-      build {
-        dependsOn(sourcesJar)
-        dependsOn(javadocJar)
-      }
+    jar {
+      define()
     }
 
-    publishing {
-      publications {
-        val publication = create<MavenPublication>("mavenJava") {
-          groupId = project.group.toString()
-          artifactId = getQualifiedProjectName()
-          version = project.version.toString()
-
-          from(components["java"])
-          artifact(tasks["sourcesJar"])
-          artifact(tasks["javadocJar"])
-          pom {
-            name.set("Salmi")
-            description.set("Fully Customizable Tab plugin for Velocity servers.")
-            url.set("https://infumia.com.tr/")
-            licenses {
-              license {
-                name.set("MIT License")
-                url.set("https://mit-license.org/license.txt")
-              }
-            }
-            developers {
-              developer {
-                id.set("portlek")
-                name.set("Hasan Demirtaş")
-                email.set("utsukushihito@outlook.com")
-              }
-            }
-            scm {
-              connection.set("scm:git:git://github.com/infumia/salmi.git")
-              developerConnection.set("scm:git:ssh://github.com/infumia/salmi.git")
-              url.set("https://github.com/infumia/salmi")
-            }
-          }
-        }
-
-        signing {
-          isRequired = signRequired
-          if (isRequired) {
-            useGpgCmd()
-            sign(publication)
-          }
-        }
-      }
+    build {
+      dependsOn(jar)
     }
+  }
+
+  repositories {
+    mavenCentral()
+    maven(SNAPSHOTS)
+    maven(PAPERMC)
+    maven(SPONGEPOWERED)
+    mavenLocal()
   }
 }
 
---
+++ plugin/build.gradle.kts
@@ -0,0 +1,18 @@
+dependencies {
+  implementation(project(":api"))
+
+  implementation(lettuceLibrary)
+  implementation(configurateJacksonLibrary)
+  implementation(jacksonDatabindLibrary)
+}
+
+tasks {
+  processResources {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    from(project.the<SourceSetContainer>()["main"].resources.srcDirs) {
+      expand("pluginVersion" to project.version)
+      include("velocity-plugin.json")
+      include("plugin.yml")
+    }
+  }
+}
